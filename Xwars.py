3586f490bd069aae30c03c173e34ecb5da9fb1f1
Denjaka <43498952+InexBots@users.noreply.github.com>
2020-02-21 11:12:44 +0700

    Add files via upload

---
+++ Xwars.py
@@ -0,0 +1,14 @@
+from wariors import LIPRO
+import json, threading, codecs
+def login(name, auth):
+    bot = LIPRO(name, auth)
+
+
+threading.Thread(target=login, args=('b1','EOeRfMdjQZQH9aq5AW04.RvBC4+PnBXbgxMCgeKJLTa.8z2Vs7BcL9e7ovD4M8Ut+tgcsyUBAPx2ngfH2Nvn4dE=')).start()
+threading.Thread(target=login, args=('b2','EOQD1tCYYG9XWvNdP1Fe.0/G8nXsnkwW7fYGdyikipG.LWc76icOsT+ECEJ7R9Q0F9JT2O8WyyQqDlTDNGAMW08=')).start()
+threading.Thread(target=login, args=('b3','EON22C95uVeXdCbqRZD9.+g24zPGUJI8VZ8kMb9Fjoq.kxBpkfXqgrFyQTJRkercO5RaBn8KxHGN7wSXcqZaYSE=')).start()
+threading.Thread(target=login, args=('b4','EORspU9YiFYTXCI5AUs3.XNJJ3NTObUsBiX2+WFfiyW.YTNzVR73ilnbKpkHrgII9PpFLcRSEDjAdBy2PKycwTo=')).start()
+threading.Thread(target=login, args=('b5','EOFSYDoFWBiaSwcq34C6.TNTGe8ZYh3eJnMfQSIpADG.KLPpEDGvbC0TAuayAn60Xcfn6Oq06LXN6c/TQP0qJ9A=')).start()
+
+print("\n___________________________________________")
+print("Login sukses")

9ba73fc532b04c4d471ab8d0e6dabdc0d9c3319f
Denjaka <43498952+InexBots@users.noreply.github.com>
2020-02-21 11:15:12 +0700

    Update Xwars.py

--- Xwars.py
+++ Xwars.py
@@ -1,7 +1,7 @@
 from wariors import Xbots
 import json, threading, codecs
 def login(name, auth):
-    bot = Xbits(name, auth)
+    bot = Xbots(name, auth)
 
 
 threading.Thread(target=login, args=('b1','TOKEN_BOT')).start()
